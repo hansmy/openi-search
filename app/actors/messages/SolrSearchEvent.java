8aac389e4b3dbd69dd20c910ca196149f9046fba
Juan David Millan <juanmillan85@gmail.com>
2014-04-29 07:54:53 -0700

    Licence More

--- app/actors/messages/SolrSearchEvent.java
+++ app/actors/messages/SolrSearchEvent.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *******************************************************************************/
+ 
 package actors.messages;
 
 public class SolrSearchEvent implements SolrEvent{
